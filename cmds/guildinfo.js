50837b8cf55d7ef8b5ecfda4aa03c4d4bd1f2126
GJaswin <jaswinkumarnr@gmail.com>
2021-04-10 09:15:41 +0530

    date format update

--- cmds/guildinfo.js
+++ cmds/guildinfo.js
@@ -5,14 +5,16 @@ module.exports = {
     description: "Shows server info",
     guildOnly: true,
     execute(txt, args) {
-        
+        var months = ["January", "February", "March", "April", "May", "June", "July", "August", "September", "October", "November", "December"];
+        var creationdate = `${txt.channel.guild.createdAt.getDate()} ${months[txt.channel.guild.createdAt.getMonth()]} ${txt.channel.guild.createdAt.getFullYear()} at ${txt.channel.guild.createdAt.getHours()}:${txt.channel.guild.createdAt.getMinutes()}:${txt.channel.guild.createdAt.getSeconds()}`;
+                
         const serverinfoembed = new Discord.MessageEmbed()
         .setColor('#ff6008')
         .setTitle('Server Info: ' + txt.channel.guild.name)
         .setThumbnail(txt.channel.guild.iconURL('png'))
         .setDescription(`Owner: **${txt.channel.guild.owner.user.tag}** \n Owner ID: \`${txt.channel.guild.ownerID}\``)
         .addFields(
-            {name: 'Created on', value: txt.channel.guild.createdAt },
+            {name: 'Created on', value: creationdate },
             { name: 'Server Member Count', value: txt.channel.guild.memberCount, inline: true },
                  
         );
